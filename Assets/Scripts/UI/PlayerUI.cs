eeb5cd99c27ccdda72a4e4b70a011c6dba61d9f0
Yamatchi <ussousso12@gmail.com>
2025-05-23 13:11:53 +0900

    feat : 타이머 추가
    타이머 추가

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -4,16 +4,43 @@ using UnityEngine.UI;
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private Image hpFillImg;
+    [SerializeField] private Text timeTxt;
+
     PlayerController playerController;
+    //public Image hpBar;
+
+    float fillAmount;
+    float sec;
+    int min;
     private void Start()
     {
         playerController = GameManager.Instance.PlayerController;
     }
-    public void SetHp()
+    private void Update()
     {
-        float fillAmount = playerController.CurrentHp / playerController.MaxHp;
+        fillAmount = SetHp();
+        TimeTxt();
+        //hpBar.fillAmount = SetHp();
+    }
+    public float SetHp()
+    {
+        fillAmount = playerController.CurrentHp / playerController.MaxHp;
 
         if (hpFillImg != null)
             hpFillImg.fillAmount = fillAmount;
+
+        return fillAmount;
     }
+    public void TimeTxt()
+    {
+        sec += Time.deltaTime;
+
+        if(sec >= 60f)
+        {
+            min += 1;
+            sec = 0;
+        }
+        timeTxt.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
+    }
+
 }
